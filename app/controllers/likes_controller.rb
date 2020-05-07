e5dcae1fec1a409650da2da573848412931ce409
jeremyhallr <jeremy.h8@hotmail.fr>
2020-05-07 18:47:52 +0200

    like update

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,18 +3,30 @@ class LikesController < ApplicationController
   end
 
   def create
-    @like = Like.new(user: current_user.id, gossip: params[:id])
+    @like = Like.new(user_id: current_user.id, gossip_id: params[:gossip_id])
 
-    if @Like.save
+    if @like.save
       puts "Operation success"
-      flash[:success] = "Liké !"
-      redirect_gossips_path
+      flash[:success] = "Création d'un nouveau potin réussie"
+      redirect_to gossips_path
     else
       puts "Failure"
-      flash[:failure] = "Like non pris en compte"
-      redirect_gossips_path
+      flash[:failure] = "Formulaire invalide"
+      redirect_to gossips_path
+    end
+  end
+
+  def index
   end
 
   def destroy
+    @like = Like.find(params[:id])
+    if current_user
+      @like.destroy
+      redirect_to gossips_path
+    else
+      flash[:failure] = "Non autorisé"
+      redirect_to gossips_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
-  resources :gossips
   resources :cities, only: [:show]
-  resources :likes, only: [:new, :create, :destroy]
+  resources :gossips do
+    resources :likes
+  end
 end
