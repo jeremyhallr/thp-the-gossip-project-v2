6695adbd05ee5be92d1a143d92b02e578a32dad1
jeremyhallr <jeremy.h8@hotmail.fr>
2020-05-08 16:12:16 +0200

    lenght user description removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :age, presence: true
   validates :description,
-    presence: true,
-    length: { minimum: 30 }
+    presence: true
   has_secure_password
   has_many :likes
 end
